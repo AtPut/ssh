5caea4d7e832b50ec26b62624b18af0e1f6fce96
unknown <atpulkka21@student.oulu.fi>
2024-10-29 17:19:02 +0200

    User Stroy 3

--- spreadsheet.py
+++ spreadsheet.py
@@ -9,6 +9,15 @@ class SpreadSheet:
 
     def get(self, cell: str) -> int | str:
         value = self._cells.get(cell, '')
+        if value.startswith('='):
+            if value.count("'") == 1:
+                return "#Error"
+            if value.count("'") == 2:
+                return value[1:]
+            try:
+                return int(float(value[1:]))
+            except ValueError:
+                return "#Error"
         if value.replace('.', '', 1).isdigit():
             if '.' in value:
                 return "#Error"
--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -18,4 +18,19 @@ class TestSpreadSheet(TestCase):
         spreadsheet = SpreadSheet()
         spreadsheet.set("A1", "'Apple")
         self.assertEqual("#Error", spreadsheet.get("A1"))
-        
+
+    def test_if_formula_string_contains_only_one_quote_mark_is_Error(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1", "='Apple")
+        self.assertEqual("#Error", spreadsheet.get("A1"))
+
+    def test_Formula_in_A1_is_1_when_decimal(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1", "=1")
+        self.assertEqual(1, spreadsheet.get("A1"))
+
+    def test_if_formula_string_contains_two_quote_mark_is_Apple(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1", "='Apple'")
+        self.assertEqual("'Apple'", spreadsheet.get("A1"))
+
